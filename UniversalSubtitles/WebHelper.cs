579b657a06824c1a2b2d44969a186ea522743154
Joel Martinez <joelmartinez@gmail.com>
2012-08-02 22:45:10 -0400

    Created project and added async friendly WebHelper class

---
+++ UniversalSubtitles/WebHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace UniversalSubtitles
+{
+	internal static class WebHelper
+	{
+		public async static Task<T> Json<T>(string url)
+        {
+			WebClient client = new WebClient();
+			var result = await client.DownloadDataTaskAsync(new Uri(url));
+			
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+			
+            var tcs = new TaskCompletionSource<T>();
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            
+            using (var stream = new MemoryStream(result))
+            {
+                var deserialized = serializer.ReadObject(stream);
+                return (T) deserialized;
+            }
+		}
+	}
+}
