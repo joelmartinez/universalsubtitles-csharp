9ea11913d0855cdd639ed14e35624890c412ffba
Joel Martinez <joelmartinez@gmail.com>
2012-08-02 23:20:35 -0400

    Added Languages method

--- UniversalSubtitles/v1/Api.cs
+++ UniversalSubtitles/v1/Api.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace UniversalSubtitles.v1
@@ -13,13 +14,48 @@ namespace UniversalSubtitles.v1
 		}
 		
 		/// <summary>
-		/// Return subtitles for video.
+		/// Return subtitles for video for the default language.
 		/// </summary>
 		public async Task<SubtitleLine[]> Subtitles(string video_url)
 		{
+			return await Subtitles(video_url, string.Empty);
+		}
+		
+		/// <summary>
+		/// Return subtitles for video for the given language.
+		/// </summary>
+		public async Task<SubtitleLine[]> Subtitles(string video_url, Language language)
+		{
+			return await Subtitles(video_url, language.Code);
+		}
+		
+		/// <summary>
+		/// Return subtitles for video for the given language.
+		/// </summary>
+		public async Task<SubtitleLine[]> Subtitles(string video_url, string language)
+		{
 			string url = string.Format("https://www.universalsubtitles.org/api/1.0/subtitles/?video_url={0}", Uri.EscapeUriString(video_url));
 			
+			if (!string.IsNullOrWhiteSpace(language))
+			{
+				url = url += "&language=" + Uri.EscapeDataString(language);
+			}
+			
 			return await WebHelper.Json<SubtitleLine[]>(url);
+		}
+		
+		/// <summary>
+		/// Return information about avilable subtitles languages that have a completion status of > 0.
+		/// </summary>
+		public async Task<Language[]> Languages(string video_url)
+		{
+			string url = string.Format("https://www.universalsubtitles.org/api/1.0/subtitles/languages/?video_url={0}", Uri.EscapeUriString(video_url));
+			
+			var languages = await WebHelper.Json<Language[]>(url);
+			
+			return languages
+				.Where(l => l.Completion != "0%" && l.Completion != "0 Lines")
+				.ToArray();
 		}
 	}
 }
---
+++ UniversalSubtitles/v1/Language.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace UniversalSubtitles.v1
+{
+	/// <summary>
+	/// Information about a Language available for a given video
+	/// </summary>
+	/// <remarks>    {
+    ///    "id": 12,
+    ///    "code": "it", 
+    ///    "name": "Italian", 
+    ///    "is_original": true
+    /// }</remarks>
+	[DataContract]
+	public class Language
+	{
+		[DataMember(Name="completion")]
+		public string Completion {get;set;}
+		
+		[DataMember(Name="id")]
+		public string Id {get;set;}
+		
+		[DataMember(Name="code")]
+		public string Code {get;set;}
+		
+		[DataMember(Name="name")]
+		public string Name {get;set;}
+		
+		[DataMember(Name="is_original")]
+		public bool IsOriginal {get;set;}
+	}
+}
