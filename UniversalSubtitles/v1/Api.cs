b436ea97e29bc76b4abfbd280e5cd6eab48dc9f5
Joel Martinez <joelmartinez@gmail.com>
2012-08-02 23:04:55 -0400

    Added v1 Api, and Subtitles method

---
+++ UniversalSubtitles/v1/Api.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace UniversalSubtitles.v1
+{
+	/// <summary>
+	/// http://www.universalsubtitles.org/api/1.0/documentation/
+	/// </summary>
+	public class Api
+	{
+		public Api()
+		{
+		}
+		
+		/// <summary>
+		/// Return subtitles for video.
+		/// </summary>
+		public async Task<SubtitleLine[]> Subtitles(string video_url)
+		{
+			string url = string.Format("https://www.universalsubtitles.org/api/1.0/subtitles/?video_url={0}", Uri.EscapeUriString(video_url));
+			
+			return await WebHelper.Json<SubtitleLine[]>(url);
+		}
+	}
+}
---
+++ UniversalSubtitles/v1/SubtitleLine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace UniversalSubtitles.v1
+{
+	/// <summary>
+	/// Represents a single subtitle
+	/// </summary>
+	/// <remarks>{
+    ///     "subtitle_id": "8893438664714", 
+    ///     "text": "Welcome to the\r\npresentation on limits.", 
+    ///     "start_time": 0.90000000000000002, 
+    ///     "end_time": 3.6099999999999999, 
+    ///     "sub_order": 1.0, 
+    ///     "start_of_paragraph": false
+    /// }</remarks>
+    [DataContract]
+    public class SubtitleLine
+	{
+    	[DataMember(Name="subtitle_id")]
+    	public string Id {get;set;}
+    	
+    	[DataMember(Name="text")]
+    	public string Text {get;set;}
+    	
+    	[DataMember(Name="start_time")]
+    	public decimal StartTime {get;set;}
+    	
+    	[DataMember(Name="end_time")]
+    	public decimal EndTime {get;set;}
+    	
+    	[DataMember(Name="sub_order")]
+    	public float SubOrder {get;set;}
+    	
+    	[DataMember(Name="start_of_paragraph")]
+    	public bool IsStartOfParagraph {get;set;}
+	}
+}
